2eb32373e1460cb2d9962997d5cee3c6350f3375
Gm7860786 <gh.mustafa2099@gmail.com>
2026-01-01 17:58:26 +0500

    script.js

--- script.js
+++ script.js
@@ -1,44 +1,3 @@
-// let timeLeft = 60;
-//     let timerId;
-
-//     const timer = document.getElementById("timer");
-//     const otpInput = document.getElementById("otpInput");
-//     const resendBtn = document.getElementById("resendBtn");
-
-//     function startOtpTimer() {
-//         timeLeft = 60;
-//         timer.innerText = "01:00";
-//         otpInput.disabled = false;
-//         resendBtn.disabled = true;
-
-//         timerId = setInterval(function () {
-//             let minutes = Math.floor(timeLeft / 60);
-//             let seconds = timeLeft % 60;
-
-//             seconds = seconds < 10 ? "0" + seconds : seconds;
-
-//             timer.innerText = `0${minutes}:${seconds}`;
-
-//             if (timeLeft === 0) {
-//                 clearInterval(timerId);
-//                 timer.textContent = "OTP Expired";
-//                 timer.classList.add("expired");
-//                 otpInput.disabled = true;
-//                 resendBtn.disabled = false;
-//             }
-
-//             timeLeft--;
-//         }, 1000);
-//     }
-
-//     resendBtn.addEventListener("click", function () {
-//         startOtpTimer();
-//     });
-
-//     // Start timer on page load
-//     startOtpTimer();
-
-
 
 let timeLeft = 60;
 let timerId;
@@ -76,5 +35,6 @@ function startOtpTimer(){
 resendBtn.addEventListener('click', () => {
     startOtpTimer();
 })
+
 
 startOtpTimer()
